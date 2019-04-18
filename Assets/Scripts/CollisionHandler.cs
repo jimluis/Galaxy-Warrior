69580b656686bd7d8b75b460323552798a3bd8de
Luis Felipe Jimenez <jimluisf@gmail.com>
2019-04-18 15:41:55 -0400

    Adding CollisionHandler

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        StartDeathSequence(collider);
+    }
+
+    void StartDeathSequence(Collider collider)
+    {
+        SendMessage("OnPlayerDeath");
+        //if (collider.gameObject.tag == "Enemies")
+        //{
+        //    print("OnTriggerEnter - Player hit enemies");
+        //}
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,20 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
-public class Player : MonoBehaviour
+
+public class PlayerController : MonoBehaviour
 {
 
     float xThrow;
     float yThrow;
-    [Tooltip("In meters per sec")][SerializeField] float xSpeed = 4f;
+
+    [Header("General")]
+    [Tooltip("In meters per sec")][SerializeField] float controlSpeed = 4f;
     [Tooltip("In meters per sec")] [SerializeField] float xRange = 4f;
     [Tooltip("In meters per sec")] [SerializeField] float yRange = 2f;
 
+    [Header("Screen-Position Based")]
     [SerializeField] float positionPitchFactor = -5f;
-    [SerializeField] float controlPitchFactor = -20f;
-
     [SerializeField] float positionYawFactor = 5f;
 
+    [Header("Control-throw Based")]
+    [SerializeField] float controlPitchFactor = -20f;
     [SerializeField] float controlRollFactor = -20f;
 
 
@@ -55,13 +59,13 @@ public class Player : MonoBehaviour
 
         xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         //The xOffset is the spaceship movement per frame
-        float xOffset = xThrow * xSpeed * timeDeltaTime;
+        float xOffset = xThrow * controlSpeed * timeDeltaTime;
         float rawXpos = transform.localPosition.x + xOffset;
         float clampedXpos = Mathf.Clamp(rawXpos, -xRange, xRange);
 
 
         yThrow = CrossPlatformInputManager.GetAxis("Vertical");
-        float yOffset = yThrow * xSpeed * timeDeltaTime;
+        float yOffset = yThrow * controlSpeed * timeDeltaTime;
         float rawYpos = transform.localPosition.y + yOffset;
         float clampedYpos = Mathf.Clamp(rawYpos, -yRange, yRange);
 
@@ -69,5 +73,12 @@ public class Player : MonoBehaviour
 
 
         transform.localPosition = new Vector3(clampedXpos, clampedYpos, transform.localPosition.z);
+    }
+
+
+    //Call by string reference
+    void OnPlayerDeath()
+    {
+        print("Control frozen");
     }
 }
